8a9fd83d3c97e711bf58024d42ab7bf98117dcd7
shaimaa-zakout <63237476+shaimaa-zakout@users.noreply.github.com>
2020-05-12 06:23:13 +0300

    Add files via upload

---
+++ PHPExer.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+
+        /**
+         * f(4)+f(3)
+         * f(3)+f(2) + f(2)+f(1)                                                  
+         * f(2)+f(1) + f(1)+f(0)  + f(1) +f(0) + f(0)                                            
+         * f(1)+ f(0)                                                                        
+ //PHP | Fibonacci Series
+         */
+       
+// function fibo($num){                                      
+//     if($num<=1){
+//         return 1;
+//     }
+// else {
+//         return (fibo($num-1)+ fibo($num-2));    
+//     }
+// }
+// $number =8;
+// for ($index = 0; $index <$number; $index++) {   
+//     echo fibo($index) . "   ";
+//}
+/**
+ *  // printNumber
+ */
+   
+//        function printNumber($num) {
+//            if ($num <= 0) {
+//                return 0;
+//            } else {
+//                echo $num . " ";
+//                printNumber($num - 1);
+//            }
+//        }
+//
+//        printNumber(5);
+  
+
+//        function Fibonacci($number){ 
+//      
+//    // if and else if to generate first two numbers 
+//    if ($number == 0) 
+//        return 0;     
+//    else if ($number == 1) 
+//        return 1;     
+//      
+//    // Recursive Call to get the upcoming numbers 
+//    else
+//        return (Fibonacci($number-1) +  
+//                Fibonacci($number-2)); 
+//} 
+//  
+//// Driver Code 
+//$number = 10; 
+//for ($counter = 0; $counter < $number; $counter++){   
+//    echo Fibonacci($counter),' '; 
+//} 
+//    
+//       **************************************************************************************//
+        echo "******************************************************************************************";
+       @mysql_connect("loclhost","root","") or die("you have error in conect");
+        echo 'dkfmdkfdndncd';
+        ?>
+    </body>
+</html>
